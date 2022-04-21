bca9e6d28d47984e0773198624e8c69150b0061b
thasuka <casferreiraa@gmail.com>
2022-04-21 16:33:35 -0300

    21-04 validações do formulário

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -56,125 +56,12 @@
             </li>
           </ul>
           <!-- <transition-group> -->
-          <!-- Login Form -->
-          <form
-            v-show="tab === 'login' "
-          >
-            <!-- Email -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
-              <input type="email"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email" />
-            </div>
-            <!-- Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
-              <input type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password" />
-            </div>
-            <button type="submit"
-              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
-                hover:bg-purple-700">
-              Submit
-            </button>
-          </form>
+
+            <AppLoginForm v-if="tab === 'login'"/>
+            <AppRegisterForm v-else/>
 
 
-          <!-- Registration Form -->
-          <vee-form
-            v-show="tab === 'register' "
-            v-bind:validation-schema="schema"
-            v-on:submit="register"
-          >
-            <!-- Name -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Name</label>
-              <vee-field
-                name="name"
-                type="text"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Name"
-              />
-              <ErrorMessage class="text-red-600" name="name"/>
-            </div>
-            <!-- Email -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
-              <vee-field
-                type="email"
-                name="email"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email" />
-                <ErrorMessage class="text-red-600" name="email"/>
-            </div>
-            <!-- Age -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Age</label>
-              <vee-field
-                name="age"
-                type="number"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded" />
-                <ErrorMessage class="text-red-600" name="age"/>
-            </div>
-            <!-- Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
-              <vee-field
-                name="password"
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password" />
-                <ErrorMessage class="text-red-600" name="password"/>
-            </div>
-            <!-- Confirm Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Confirm Password</label>
-              <vee-field
-                name="confirm_password"
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Confirm Password" />
-                <ErrorMessage class="text-red-600" name="confirm_password"/>
-            </div>
-            <!-- Country -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Country</label>
-              <vee-field as="select"
-                name="country"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded">
-                <option value="USA">USA</option>
-                <option value="Mexico">Mexico</option>
-                <option value="Germany">Germany</option>
-                <option value="Antarctica">Antarctica</option>
-              </vee-field>
-               <ErrorMessage class="text-red-600" name="country"/>
-            </div>
-            <!-- TOS -->
-            <div class="mb-3 pl-6">
-              <vee-field
-              name="tos"
-              value="1"
-              type="checkbox"
-              class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
-              <label class="inline-block">Accept terms of service</label>
-              <ErrorMessage class="text-red-600 block" name="tos"/>
-            </div>
-            <button type="submit"
-              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
-                hover:bg-purple-700">
-              Submit
-            </button>
-          </vee-form>
+
 
           <!-- </transition-group> -->
         </div>
@@ -187,8 +74,16 @@
 /* eslint-disable */
 import { mapMutations, mapState  } from 'vuex';
 
+//components
+import  AppRegisterForm  from './RegisterForm.vue';
+import AppLoginForm from "./LoginForm.vue";
+
 export default {
   name: 'AuthModal',
+  components:{
+    AppLoginForm,
+    AppRegisterForm
+  },
   computed: {
     // authModalShow() {
     //   return this.$store.getters.authModalShow;
@@ -203,24 +98,14 @@ export default {
   data(){
     return{
       tab: 'login',
-      schema: {
-        name: 'required|min:3|max:100|alphaSpaces',
-        email: 'required|min:3|max:100|email',
-        age: 'required|min_value:18|max_value:99',
-        password: 'required|min:5|max:8',
-        confirm_password: 'confirmed:@password',
-        country: 'required|excluded:Antarctica',
-        tos: 'required',
-      },
     };
   },
   methods: {
 
     ...mapMutations(['toogleAuthModal']),
-    register(values){
-      console.log(values)
 
-    },
+
+
   },
 };
 </script>
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -3,6 +3,7 @@ import {
   Field as VeeField,
   defineRule,
   ErrorMessage,
+  configure,
 } from 'vee-validate';
 import {
   required,
@@ -23,13 +24,41 @@ export default {
     app.component('ErrorMessage', ErrorMessage);
 
     defineRule('required', required);
+    defineRule('tos', required);
     defineRule('min', min);
     defineRule('max', max);
     defineRule('min_value', minValue);
     defineRule('max_value', maxValue);
     defineRule('alphaSpaces', alphaSpaces);
     defineRule('email', email);
-    defineRule('confirmed', confirmed);
+    defineRule('passwords_mismatch', confirmed);
     defineRule('excluded', excluded);
+    defineRule('country_excluded', excluded);
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The field ${ctx.field} is required`,
+          min: `The field ${ctx.field} is too short`,
+          max: `The field ${ctx.field} is too long`,
+          alpha_spaces: `The field ${ctx.field} may only contain alphabetical characters and spaces`,
+          email: `The field ${ctx.field} must be a valid email`,
+          min_value: `The field ${ctx.field} is to low`,
+          max_value: `The field ${ctx.field} is to high`,
+          excluded: `You are not allowed to use this value for the field ${ctx.field}`,
+          country_excluded: 'Due to restrictions, we do not accept users from this locations',
+          passwords_mismatch: "The passwords don't match",
+          tos: 'Tou must accepts the Terms of Services',
+        };
+
+        const message = messages[ctx.rule.name] ? messages[ctx.rule.name] : `The field ${ctx.field} is invalid`;
+
+        return message;
+      },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true,
+    });
   },
 };
