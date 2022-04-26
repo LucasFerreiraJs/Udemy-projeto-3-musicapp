4f0749cdbb4987ed56b32237c6fef6f71c92efe9
thasuka <casferreiraa@gmail.com>
2022-04-25 23:09:04 -0300

    25-04 inicializando pg de comentários

---
+++ src/components/SongItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <li
+
+    class="flex justify-between items-center p-3 pl-6 cursor-pointer transition
+    duration-300 hover:bg-gray-50">
+    <div>
+      <router-link
+        v-bind:to="{ name: 'song', params: {id: songProp.docID } }"
+        class="font-bold block text-gray-600">{{ songProp.modified_name }}
+      </router-link>
+      <span class="text-gray-500 text-sm">{{ songProp.display_name}}</span>
+    </div>
+
+    <div class="text-gray-600 text-lg">
+      <span class="comments">
+        <i class="fa fa-comments text-gray-600"></i>
+        {{ songProp.comment_count }}
+      </span>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'SongItem',
+  props: ['songProp'],
+};
+</script>
